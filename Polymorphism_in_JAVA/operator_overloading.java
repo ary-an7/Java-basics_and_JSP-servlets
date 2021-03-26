70278a21bda8fcffd8eb2e4292580e535916c00b
Aryan Pratap <aryanpratap98765@gmail.com>
2021-03-26 23:09:35 +0530

    Create operator_overloading.java

---
+++ Polymorphism_in_JAVA/operator_overloading.java
@@ -0,0 +1,15 @@
+class Main 
+{
+    public static void main(String[] args)
+    {
+        int a = 3;
+        int b = 8;
+        int sum = a + b;
+        String first = "Java ";
+        String second = "Programming";
+        String name = first + second;                //operator + overloaded here
+        System.out.println("+ with numbers="+sum);
+        System.out.println("+ with strings="+name);
+
+    }
+}
